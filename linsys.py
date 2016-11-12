94e678f4542b06e2b196a0df3c8e843ad7dbb9f0
Jeremy Shannon <jeremyeshannon@gmail.com>
2016-11-12 14:07:23 -0600

    replaced and formatted test cases for computing triangular form

--- linsys.py
+++ linsys.py
@@ -65,6 +65,8 @@ class LinearSystem(object):
 
         return indices
 
+    def compute_triangular_form(self):
+        print "hi!"
 
     def __len__(self):
         return len(self.planes)
@@ -95,69 +97,40 @@ class MyDecimal(Decimal):
         return abs(self) < eps
 
 
-# p0 = Plane(normal_vector=Vector(['1','1','1']), constant_term='1')
-# p1 = Plane(normal_vector=Vector(['0','1','0']), constant_term='2')
-# p2 = Plane(normal_vector=Vector(['1','1','-1']), constant_term='3')
-# p3 = Plane(normal_vector=Vector(['1','0','-2']), constant_term='2')
-p0 = Plane([1.,1.,1.],1.)
-p1 = Plane([0.,1.,0.],2.)
-p2 = Plane([1.,1.,-1.],3.)
-p3 = Plane([1.,0.,-2.],2.)
-
-s = LinearSystem([p0,p1,p2,p3])
-
-# print s.indices_of_first_nonzero_terms_in_each_row()
-# print '{},{},{},{}'.format(s[0],s[1],s[2],s[3])
-# print len(s)
-# print s
-
-# print MyDecimal('1e-9').is_near_zero()
-# print MyDecimal('1e-11').is_near_zero()
-
-s.swap_rows(0,1)
-if not (s[0] == p1 and s[1] == p0 and s[2] == p2 and s[3] == p3):
+p1 = Plane(normal_vector=(['1','1','1']), constant_term='1')
+p2 = Plane(normal_vector=(['0','1','1']), constant_term='2')
+s = LinearSystem([p1,p2])
+t = s.compute_triangular_form()
+if not (t[0] == p1 and
+        t[1] == p2):
     print 'test case 1 failed'
 
-s.swap_rows(1,3)
-if not (s[0] == p1 and s[1] == p3 and s[2] == p2 and s[3] == p0):
+p1 = Plane(normal_vector=(['1','1','1']), constant_term='1')
+p2 = Plane(normal_vector=(['1','1','1']), constant_term='2')
+s = LinearSystem([p1,p2])
+t = s.compute_triangular_form()
+if not (t[0] == p1 and
+        t[1] == Plane(constant_term='1')):
     print 'test case 2 failed'
 
-s.swap_rows(3,1)
-if not (s[0] == p1 and s[1] == p0 and s[2] == p2 and s[3] == p3):
+p1 = Plane(normal_vector=(['1','1','1']), constant_term='1')
+p2 = Plane(normal_vector=(['0','1','0']), constant_term='2')
+p3 = Plane(normal_vector=(['1','1','-1']), constant_term='3')
+p4 = Plane(normal_vector=(['1','0','-2']), constant_term='2')
+s = LinearSystem([p1,p2,p3,p4])
+t = s.compute_triangular_form()
+if not (t[0] == p1 and
+        t[1] == p2 and
+        t[2] == Plane(normal_vector=(['0','0','-2']), constant_term='2') and
+        t[3] == Plane()):
     print 'test case 3 failed'
 
-s.multiply_coefficient_and_row(1,0)
-if not (s[0] == p1 and s[1] == p0 and s[2] == p2 and s[3] == p3):
+p1 = Plane(normal_vector=(['0','1','1']), constant_term='1')
+p2 = Plane(normal_vector=(['1','-1','1']), constant_term='2')
+p3 = Plane(normal_vector=(['1','2','-5']), constant_term='3')
+s = LinearSystem([p1,p2,p3])
+t = s.compute_triangular_form()
+if not (t[0] == Plane(normal_vector=(['1','-1','1']), constant_term='2') and
+        t[1] == Plane(normal_vector=(['0','1','1']), constant_term='1') and
+        t[2] == Plane(normal_vector=(['0','0','-9']), constant_term='-2')):
     print 'test case 4 failed'
-
-s.multiply_coefficient_and_row(-1,2)
-if not (s[0] == p1 and s[1] == p0 and s[2] == Plane([-1,-1,1],-3) and s[3] == p3):
-    print 'test case 5 failed'
-
-s.multiply_coefficient_and_row(10,1)
-if not (s[0] == p1 and
-        s[1] == Plane([10,10,10],10) and
-        s[2] == Plane([-1,-1,1],-3) and
-        s[3] == p3):
-    print 'test case 6 failed'
-
-s.add_multiple_times_row_to_row(0,0,1)
-if not (s[0] == p1 and
-        s[1] == Plane([10,10,10], 10) and
-        s[2] == Plane([-1,-1,1], -3) and
-        s[3] == p3):
-    print 'test case 7 failed'
-
-s.add_multiple_times_row_to_row(1,0,1)
-if not (s[0] == p1 and
-        s[1] == Plane([10,11,10], 12) and
-        s[2] == Plane([-1,-1,1], -3) and
-        s[3] == p3):
-    print 'test case 8 failed'
-
-s.add_multiple_times_row_to_row(-1,1,0)
-if not (s[0] == Plane([-10,-10,-10], -10) and 
-        s[1] == Plane([10,11,10], 12) and 
-        s[2] == Plane([-1,-1,1], -3) and 
-        s[3] == p3):
-    print 'test case 9 failed'
