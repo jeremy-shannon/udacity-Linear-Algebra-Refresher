e353fce4ae9de03d7784e8e47edae347c634fc40
Jeremy Shannon <jeremyeshannon@gmail.com>
2016-11-13 10:25:10 -0600

    swapped test cases for Coding Gaussian Elimination

--- linsys.py
+++ linsys.py
@@ -132,47 +132,25 @@ class MyDecimal(Decimal):
         return abs(self) < eps
 
 print "********************test case 1 *******************************"
-p1 = Plane(normal_vector=(['1','1','1']), constant_term='1')
-p2 = Plane(normal_vector=(['0','1','1']), constant_term='2')
+p1 = Plane(normal_vector=(['5.862','1.178','-10.366']), constant_term='-8.15')
+p2 = Plane(normal_vector=(['2.931','-0.589','5.183']), constant_term='-4.075')
 s = LinearSystem([p1,p2])
 r = s.compute_rref()
-if not (r[0] == Plane(normal_vector=(['1','0','0']), constant_term='-1') and
-        r[1] == p2):
-    print 'test case 1 failed'
 print r
 
 print "********************test case 2 *******************************"
-p1 = Plane(normal_vector=(['1','1','1']), constant_term='1')
-p2 = Plane(normal_vector=(['1','1','1']), constant_term='2')
-s = LinearSystem([p1,p2])
+p1 = Plane(normal_vector=(['8.631','5.112','-1.816']), constant_term='-5.113')
+p2 = Plane(normal_vector=(['4.315','11.132','-5.27']), constant_term='-6.775')
+p3 = Plane(normal_vector=(['-2.158','3.01','-1.727']), constant_term='-0.831')
+s = LinearSystem([p1,p2,p3])
 r = s.compute_rref()
-if not (r[0] == p1 and
-        r[1] == Plane(constant_term='1')):
-    print 'test case 2 failed'
 print r
 
 print "********************test case 3 *******************************"
-p1 = Plane(normal_vector=(['1','1','1']), constant_term='1')
-p2 = Plane(normal_vector=(['0','1','0']), constant_term='2')
-p3 = Plane(normal_vector=(['1','1','-1']), constant_term='3')
-p4 = Plane(normal_vector=(['1','0','-2']), constant_term='2')
+p1 = Plane(normal_vector=(['5.262','2.739','-9.878']), constant_term='-3.441')
+p2 = Plane(normal_vector=(['5.111','6.358','7.638']), constant_term='-2.152')
+p3 = Plane(normal_vector=(['2.016','-9.924','-1.367']), constant_term='-9.278')
+p4 = Plane(normal_vector=(['2.167','-13.543','-18.883']), constant_term='-10.567')
 s = LinearSystem([p1,p2,p3,p4])
 r = s.compute_rref()
-if not (r[0] == Plane(normal_vector=(['1','0','0']), constant_term='0') and
-        r[1] == p2 and
-        r[2] == Plane(normal_vector=(['0','0','-2']), constant_term='2') and
-        r[3] == Plane()):
-    print 'test case 3 failed'
-print r
-
-print "********************test case 4 *******************************"
-p1 = Plane(normal_vector=(['0','1','1']), constant_term='1')
-p2 = Plane(normal_vector=(['1','-1','1']), constant_term='2')
-p3 = Plane(normal_vector=(['1','2','-5']), constant_term='3')
-s = LinearSystem([p1,p2,p3])
-r = s.compute_rref()
-if not (r[0] == Plane(normal_vector=(['1','0','0']), constant_term=Decimal('23')/Decimal('9')) and
-        r[1] == Plane(normal_vector=(['0','1','0']), constant_term=Decimal('7')/Decimal('9')) and
-        r[2] == Plane(normal_vector=(['0','0','1']), constant_term=Decimal('2')/Decimal('9'))):
-    print 'test case 4 failed'
 print r
